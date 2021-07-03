b3ddcf45574f5b39a98b718b7019dbe004714b5c
Derek Surguy <Surguy.Derek@gmail.com>
2021-07-03 13:50:41 -0500

    Seed the bingo lines for testing, add a progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
-import { TextField, Container, Button, Box } from '@material-ui/core'
+import React, { MouseEvent, useState } from 'react'
+import { TextField, Container, Button, Box, LinearProgress } from '@material-ui/core'
+import lineSeedWords from './lipsumSeed';
+import { getRandomIntInclusive } from './utils/random';
 import './App.css'
 
 function App() {
-  const [lines, setLines] = useState("")
+  const lineSeed = new Array(25).fill("").map(stub => {
+    const numberOfWords = getRandomIntInclusive(1, 8);
+    return new Array(numberOfWords).fill("").map(innerStub => lineSeedWords[getRandomIntInclusive(0, lineSeedWords.length - 1)]).join(' ');
+  }).join("\n");
+  const [lines, setLines] = useState(lineSeed)
+  const [progress, setProgress] = useState(0)
+  const [inProgress, setInProgress] = useState(false);
 
   const onBingoLinesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLines(e.target.value)
+  }
+
+  const onDoTheThingClick = (e: MouseEvent) => {
+    setInProgress(true);
+    setProgress(0);
   }
 
   return (
@@ -22,11 +35,24 @@ function App() {
           value={lines}
           onChange={onBingoLinesChange}
           helperText="Enter one bingo action per line"
+          fullWidth
+          InputProps={{
+            fullWidth: true
+          }}
         ></TextField>
       </Box>
       <Box className="actions" marginTop={2}>
-        <Button variant="contained" color="primary">Do the thing</Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onDoTheThingClick}
+        >
+          Do the thing
+        </Button>
       </Box>
+      {inProgress && (<Box className="progress" marginTop={2}>
+        <LinearProgress variant="determinate" value={progress} />
+      </Box>)}
     </Container>
   )
 }
---
+++ src/lipsumSeed.ts
@@ -0,0 +1,18 @@
+const seed = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis fermentum semper. Sed sit amet egestas mauris, eu tincidunt libero. In in erat vitae lectus venenatis sollicitudin. Nullam pellentesque tellus a magna ultricies, ac tempor leo ultrices. Ut sit amet nibh iaculis justo lobortis finibus bibendum sit amet tellus. In id bibendum massa. Proin quis consectetur diam. Ut porttitor tortor a orci vestibulum congue eu ut ipsum. Integer egestas a arcu sed iaculis. Nullam lacinia euismod ligula ac sagittis. Nullam et erat sed nisi placerat consequat ut quis erat. Ut at purus cursus, suscipit lacus in, aliquam arcu.
+
+Sed eu tellus bibendum, laoreet orci ultrices, malesuada libero. Maecenas bibendum, tortor quis efficitur dapibus, ex quam facilisis felis, id mollis metus nunc ac ligula. Nam quis est consectetur, rutrum ipsum sit amet, sollicitudin urna. Etiam vulputate vestibulum ante quis dignissim. Fusce at bibendum velit. Vivamus eu vestibulum purus. Quisque est est, eleifend quis tempor fermentum, convallis non metus. Nullam in gravida velit.
+
+Curabitur eu nisl sem. Nam diam dolor, tempus vitae massa at, placerat malesuada magna. Ut aliquam nec quam non tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non lectus eget tellus consectetur luctus a ut justo. Vestibulum semper neque felis, ut cursus mauris malesuada at. Sed ut tortor et justo dignissim ultrices nec nec leo.
+
+Vivamus at est aliquet, congue mi eget, accumsan velit. Mauris tristique ac nulla ac ultricies. Sed nec molestie neque, et tincidunt elit. Nulla scelerisque nisl est, ac hendrerit libero vehicula at. Nunc in arcu vestibulum, facilisis velit ut, venenatis neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed est ligula, consectetur nec rutrum ac, eleifend ac urna.
+
+Etiam ultricies dapibus felis efficitur volutpat. Donec finibus metus sed consectetur fringilla. Vivamus quis mauris elit. Integer pretium enim tortor, non ultrices elit viverra ac. Fusce dolor risus, auctor eget enim vitae, dignissim facilisis libero. Mauris ac tellus fermentum, pretium sapien non, viverra dui. Nulla nec malesuada lacus.
+
+Donec pulvinar metus eget pulvinar laoreet. Pellentesque sagittis molestie tortor, vitae viverra dolor ornare non. Vivamus ut ipsum venenatis tortor ultricies vehicula. Nunc ornare purus ut purus semper, quis malesuada quam tincidunt. Aliquam maximus, leo id ullamcorper aliquam, lectus odio iaculis leo, ac consequat risus orci non quam. Sed sit amet metus nec diam pharetra blandit sit amet non purus. Vestibulum eu leo luctus dui condimentum feugiat. Nam at enim consectetur, efficitur nisi in, tincidunt lorem. Curabitur eu volutpat dolor, laoreet tristique arcu. Nulla elementum tempor scelerisque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam erat volutpat. Suspendisse lobortis nisl quis magna eleifend sodales. Fusce rhoncus enim id leo ultricies, vel pharetra purus pharetra.
+
+Nulla facilisi. Vivamus commodo tortor massa, et ullamcorper felis aliquam luctus. Curabitur placerat auctor luctus. Morbi eros enim, viverra sed tristique vel, cursus quis nulla. Sed imperdiet laoreet ultricies. Quisque ac iaculis lorem, eu maximus odio. In hac habitasse platea dictumst. Praesent sodales nibh nisi, ac elementum dolor interdum a. Aliquam.
+`;
+
+const words = seed.replace(/[^\d\w\s]/g, '').replace(/\s/g, ' ').split(/\s/).filter(word => word);
+export default words;

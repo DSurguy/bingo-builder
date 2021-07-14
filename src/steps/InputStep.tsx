30cab42aa7b965e400704fb5142fd00af92187ca
Derek Surguy <Surguy.Derek@gmail.com>
2021-07-14 08:41:11 -0500

    Update instructions and pad to minimum 25 lines

--- src/steps/InputStep.tsx
+++ src/steps/InputStep.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent, useState } from 'react'
+import React, { MouseEvent, useState, useEffect } from 'react'
 import { TextField, Button, Box, Container, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core'
 import lineSeedWords from '../lipsumSeed';
 import { getRandomIntInclusive } from '../utils/random';
@@ -14,6 +14,7 @@ export default function InputStep({ onComplete }: Props) {
     return new Array(numberOfWords).fill("").map(() => lineSeedWords[getRandomIntInclusive(0, lineSeedWords.length - 1)]).join(' ');
   }).join("\n");
   const [lines, setLines] = useState(lineSeed)
+  const [numLines, setNumLines] = useState(25)
   const [freeSpaceSetting, setFreeSpaceSetting] = useState(FreeSpaceSetting.none)
 
   const onBingoLinesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,31 +22,50 @@ export default function InputStep({ onComplete }: Props) {
   }
 
   const onNextClick = (e: MouseEvent) => {
+    const paddedLines = lines.split(/[\n\r]/g).map(line => line.trim()).filter(l => l);
+    while(paddedLines.length < 25) paddedLines.push("");
+
     onComplete({
-      lines: lines.split(/\n/g).map(line => line.trim()),
+      lines: paddedLines,
       freeSpaceSetting
     });
   }
 
+  useEffect(() => {
+    setNumLines(lines.split(/[\n\r]/g).map(line => line.trim()).filter(l => l).length)
+  }, [lines])
+
   return (
     <Container>
       <Box className="intro" marginTop={2}>
-        <h2>I am the input step</h2>
+        <h1>Bingo Builder</h1>
+        <Box>
+          <h2>Instructions</h2>
+          <p>
+            Please enter as many bingo square entries as you like below, one per line. Empty lines will be ignored.
+          </p>
+          <p>
+            At this time, the bingo board is limited to 5x5 (25) squares. Each board will use a random selection of 
+            25 of the lines below. If fewer than 25 lines are provided, empty spaces will be used to fill the rest
+            of the squares.
+          </p>
+        </Box>
       </Box>
       <Box marginTop={2}>
         <TextField
           id="bingoLines"
-          label="Bingo Lines"
+          label={`Bingo Lines (${numLines})`}
           multiline
           value={lines}
           onChange={onBingoLinesChange}
-          helperText="Enter one bingo action per line"
           fullWidth
           InputProps={{
             fullWidth: true
           }}
           variant="outlined"
         ></TextField>
+      </Box>
+      <Box marginTop={2}>
         <FormControl component="fieldset">
           <FormLabel component="legend">Free Space Position</FormLabel>
           <RadioGroup aria-label="Free Space Position" name="freeSpacePosition" value={freeSpaceSetting} onChange={e => setFreeSpaceSetting(e.target.value as FreeSpaceSetting)}>
@@ -55,7 +75,7 @@ export default function InputStep({ onComplete }: Props) {
           </RadioGroup>
         </FormControl>
       </Box>
-      <Box className="actions" marginTop={2}>
+      <Box className="actions" marginTop={2} marginBottom={2}>
         <Button
           variant="contained"
           color="primary"
